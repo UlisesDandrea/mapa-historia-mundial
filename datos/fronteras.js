a70e0e827b6420adbf088087ddb4b72868285bc0
UlisesDandrea <ulisesdogo2004@gmail.com>
2026-08-11 16:22:19 -0300

    Add files via upload

---
+++ datos/fronteras.js
@@ -0,0 +1,31 @@
+const fronterasHistoricas = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "nombre": "Imperio Romano de Occidente",
+        "año_inicio": -27,
+        "año_fin": 476,
+        "color": "#9B59B6"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-5.0, 36.0], [5.0, 36.0], [15.0, 38.0], [20.0, 45.0], [10.0, 50.0], [-5.0, 48.0], [-5.0, 36.0]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "nombre": "Imperio Otomano (Máxima Extensión)",
+        "año_inicio": 1453,
+        "año_fin": 1922,
+        "color": "#27AE60"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[20.0, 35.0], [30.0, 32.0], [45.0, 30.0], [45.0, 40.0], [35.0, 45.0], [20.0, 42.0], [20.0, 35.0]]]
+      }
+    }
+  ]
+};
